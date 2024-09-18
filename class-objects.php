7a418f791cd55052705bd9296f703e32df4c7ffc
Chandra Putera <Chandrawpsiloto@gmail.com>
2024-09-18 22:41:47 +0700

    Membuat class dari objects

--- class-objects.php
+++ class-objects.php
@@ -3,36 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type=text/css>
+    <title>Tesalonika</title>
 </head>
 <body>
 
 <?php
-// Mendefinisikan kelas Car
-class Car {
-    // Mendeklarasikan properti
-    public $make; // Merek mobil
-    public $model; // Model mobil
-    public $year; // Tahun pembuatan mobil
+    // // Mendefinisikan kelas Car
+    // class Car {
+    //     // Mendeklarasikan properti
+    //     public $make; // Merek mobil
+    //     public $model; // Model mobil
+    //     public $year; // Tahun pembuatan mobil
 
-    // Mendefinisikan konstruktor
-    public function __construct($make, $model, $year) {
-        // Menginisialisasi properti dengan nilai yang diberikan
-        $this->make = $make; // Mengatur properti make
-        $this->model = $model; // Mengatur properti model
-        $this->year = $year; // Mengatur properti year
+    //     // Mendefinisikan konstruktor
+    //     public function __construct($make, $model, $year) {
+    //         // Menginisialisasi properti dengan nilai yang diberikan
+    //         $this->make = $make; // Mengatur properti make
+    //         $this->model = $model; // Mengatur properti model
+    //         $this->year = $year; // Mengatur properti year
+    //     }
+
+    //     // Mendefinisikan metode
+    //     public function getCarInfo() {
+    //         // Mengembalikan informasi mobil dalam format string
+    //         return $this->year . ' ' . $this->make . ' ' . $this->model;
+    //     }
+    // }
+
+    // // Contoh penggunaan kelas Car
+    // $car1 = new Car("Toyota", "Corolla", 2020); // Membuat objek Car baru
+    // echo $car1->getCarInfo(); // Menampilkan informasi mobil
+
+
+    class Produk {
+
+
     }
 
-    // Mendefinisikan metode
-    public function getCarInfo() {
-        // Mengembalikan informasi mobil dalam format string
-        return $this->year . ' ' . $this->make . ' ' . $this->model;
-    }
-}
+    $produk1 = new Produk();
+    $produk2 = new Produk();
 
-// Contoh penggunaan kelas Car
-$car1 = new Car("Toyota", "Corolla", 2020); // Membuat objek Car baru
-echo $car1->getCarInfo(); // Menampilkan informasi mobil
 ?>
 
 
